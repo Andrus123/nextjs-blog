4fd4edecbdedc7df8dd9fc0e193aac53c994f9f5
Andrés K. Aquin <andruuus123@gmail.com>
2021-09-30 12:48:44 -0400

    Social Media

--- components/socialButtons.js
+++ components/socialButtons.js
@@ -4,10 +4,10 @@ import utilStyles from '../styles/utils.module.css'
 function SocialButtons() {
     return (
         <div className={utilStyles.socialSection}>
-            <a className={utilStyles.social}><img src="../images/github-brands.svg"/></a>
+            <a href="https://github.com/Andrus123" className={utilStyles.social}><img src="../images/github-brands.svg"/></a>
             <a className={utilStyles.social}><img src="../images/facebook-brands.svg"/></a>
-            <a className={utilStyles.social}><img src="../images/linkedin-brands.svg"/></a>
-            <a className={utilStyles.social}><img src="../images/envelope-solid.svg"/></a>
+            <a href="https://www.linkedin.com/in/andr%C3%A9s-aquin-43a122141/" className={utilStyles.social}><img src="../images/linkedin-brands.svg"/></a>
+            <a href="akaquin@umsa.bo" className={utilStyles.social}><img src="../images/envelope-solid.svg"/></a>
         </div>
     )
 }
